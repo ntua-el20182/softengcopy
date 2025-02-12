79af572e2dd871f36ea707acf4dfa087f535f6de
ntua-el20162 <el20162@mail.ntua.gr>
2025-02-12 22:39:41 +0200

    Update build_modules.bat

--- back-end/build_modules.bat
+++ back-end/build_modules.bat
@@ -1,5 +1,5 @@
 :: Εγκατάσταση dependencies
 echo 📌 Εγκατάσταση dependencies...
-npm install express dotenv mysql2 cors jsonwebtoken path fs body-parser querystring assert dayjs multer csv-parser swagger-jsdoc swagger-ui-express
+npm install express dotenv mysql2 cors jsonwebtoken path fs body-parser querystring assert dayjs multer csv-parser swagger-jsdoc swagger-ui-express commander
 echo ✅ Όλα τα πακέτα εγκαταστάθηκαν!
 pause
