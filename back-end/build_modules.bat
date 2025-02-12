b5a258ece041b2c62ce012fcfeb01624ccd15628
ntua-el20162 <el20162@mail.ntua.gr>
2025-02-12 22:42:24 +0200

    Update build_modules.bat

--- back-end/build_modules.bat
+++ back-end/build_modules.bat
@@ -1,5 +1,11 @@
 :: Εγκατάσταση dependencies
 echo 📌 Εγκατάσταση dependencies...
 npm install express dotenv mysql2 cors jsonwebtoken path fs body-parser querystring assert dayjs multer csv-parser swagger-jsdoc swagger-ui-express commander
+
+:: Εγκατάσταση axios και commander στον φάκελο ../cli-client
+echo 📦 Εγκατάσταση axios και commander στο ../cli-client...
+cd ..\cli-client
+npm install axios commander
+
 echo ✅ Όλα τα πακέτα εγκαταστάθηκαν!
 pause
