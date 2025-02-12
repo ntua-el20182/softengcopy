64b7801f5023b47b8d28af0f6233d68c541e705d
ntua-el20182 <el20182@mail.ntua.gr>
2025-02-12 19:01:30 +0200

    Update swagger.js

--- back-end/swagger.js
+++ back-end/swagger.js
@@ -1,6 +1,6 @@
 const swaggerJSDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
-const config = require('../config'); // Import the configuration
+const config = require('../front-end/config'); // Import the configuration
 const baseURL = config.baseURL; // Get the baseURL from the configuration
 
 const swaggerOptions = {
